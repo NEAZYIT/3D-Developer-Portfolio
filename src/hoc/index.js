25c76554aa9f0b6790893ccb782f37873fdf346b
NEAZYIT <touihnazih28@gmail.com>
2024-05-27 23:09:43 +0100

    feat: Implement Reusable Section Wrapper with Animations
    This commit introduces a reusable `SectionWrapper` Higher-Order Component (HOC) designed to enhance the visual appeal and maintainability of the website sections through smooth animations and unique IDs for easy navigation.
    
    Key Improvements:
    - **Enhanced Visual Appeal**: Sections now smoothly fade into view upon scrolling, offering a more engaging user experience.
    - **Improved Code Structure**: The `SectionWrapper` promotes code reusability by abstracting common section logic such as animations and IDs.
    - **Simplified Imports**: The component is re-exported from a central location for easier access and cleaner import statements.
    
    This change significantly improves the website's aesthetics and maintainability by providing a consistent, reusable approach to styling and animating sections.

---
+++ src/hoc/index.js
@@ -0,0 +1,12 @@
+import SectionWrapper from "./SectionWrapper"; // Importing the SectionWrapper component from its file location.
+
+/**
+ * Re-exporting the `SectionWrapper` component.
+ *
+ * This file acts like a central hub, making it easy to import the `SectionWrapper`
+ * from one place instead of remembering its exact file location every time.
+ *
+ * It's like having a shortcut on your desktop that takes you directly to
+ * an important file!
+ */
+export { SectionWrapper };
